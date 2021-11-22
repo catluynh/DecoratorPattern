d09a82b2b46a5530f9110449226089e62677916f
catluynh <catluynh99@gmail.com>
2021-11-22 15:53:31 +0700

    first commit

---
+++ src/CoffeeType/DarkRoast.java
@@ -0,0 +1,11 @@
+package CoffeeType;
+
+public class DarkRoast extends Beverage{
+
+    public DarkRoast() {
+        description = "DarkRoast Coffee";
+    }
+    public double cost() {
+        return 1.05;
+    }
+}
---
+++ src/Condiment/CondimentDecorator.java
@@ -0,0 +1,7 @@
+package Condiment;
+
+import CoffeeType.Beverage;
+
+public abstract class CondimentDecorator extends Beverage {
+    public abstract String getDescription();
+}
